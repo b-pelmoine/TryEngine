52534c5bfb77f022e66feb0adff63d2f6028e052
Tero <contact.tero@gmail.com>
2018-10-05 15:16:39 +0200

    Add Event dispatching for event defined by the user

--- include/TryEngine/Window/TEInput.hpp
+++ include/TryEngine/Window/TEInput.hpp
@@ -2,18 +2,47 @@
 #define TERO_INPUTHANDLER_HPP
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+
+#include <functional>
+#include <unordered_map>
+#include <vector>
+
+using TEInputListener = std::function<void(const sf::Event&)>;
+using TEInputListenerEntry = std::pair<TEInputListener, size_t>;
+
+struct TEInputDescriptor
+{
+    TEInputDescriptor(std::string _identifier, sf::Event::EventType _type, std::function<bool(const sf::Event&)> _predicate):
+    identifier(_identifier), type(_type), predicate(_predicate)
+    {}
+    private:
+    std::string identifier;
+    sf::Event::EventType type;
+    std::function<bool(const sf::Event&)> predicate;
+    std::vector<TEInputListenerEntry> listeners;
+
+    void Call(const sf::Event& event) { for(const auto& listener : listeners) listener.first(event); }
+
+    friend class TEInput;
+};
 
 class TEInput
 {
     public:
-    TEInput();
+    TEInput() : bQueryingClose(false) {}
 
     void Handle(sf::Event& event);
     
-    bool ReceivedCloseEvent() const { return m_queryingClose; }
+    bool ReceivedCloseEvent() const { return bQueryingClose; }
+
+    void RegisterInputDescriptor(TEInputDescriptor&&);
+    size_t AddListener(const std::string& identifier, const TEInputListener& listener);
+    void RemoveListener(const std::string& identifier, const size_t& ID);
 
     private:
-    bool m_queryingClose;
+    bool bQueryingClose;
+    std::unordered_map<std::string, TEInputDescriptor> m_registeredInputs;
 };
 
 #endif
--- src/TryEngine/Window/TEInput.cpp
+++ src/TryEngine/Window/TEInput.cpp
@@ -1,11 +1,42 @@
 #include "Window/TEInput.hpp"
 #include <SFML/Window/Event.hpp>
-
-TEInput::TEInput()
-{}
+#include <assert.h>
 
 void TEInput::Handle(sf::Event& event)
 {
+    for(auto entry : m_registeredInputs)
+    {
+        TEInputDescriptor& t_descriptor = entry.second;
+        if(event.type == t_descriptor.type && t_descriptor.predicate(event))
+            t_descriptor.Call(event);
+    }
     if(event.type == sf::Event::EventType::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-        m_queryingClose = true;
+        bQueryingClose = true;
 }
+
+void TEInput::RegisterInputDescriptor(TEInputDescriptor&& descriptor)
+{
+    m_registeredInputs.insert(std::make_pair(descriptor.identifier, descriptor));
+}
+
+size_t TEInput::AddListener(const std::string& identifier, const TEInputListener& listener)
+{
+    auto it = m_registeredInputs.find(identifier);
+    assert(it != m_registeredInputs.end());
+    auto& listeners = it->second.listeners;
+    size_t ID = listeners.empty() ? 0 : listeners.back().second + 1;
+    TEInputListenerEntry entry = std::make_pair(listener , ID);
+    listeners.push_back(entry);
+    return ID;
+}
+
+void TEInput::RemoveListener(const std::string& identifier, const size_t& ID)
+{
+    auto it = m_registeredInputs.find(identifier);
+    assert(it != m_registeredInputs.end());
+    auto& listeners = it->second.listeners;
+    auto element = std :: remove_if (listeners.begin(), listeners.end() , [ID](const TEInputListenerEntry& listener){ return listener.second == ID; });
+    listeners.erase(element, listeners.end());
+}
+
+
