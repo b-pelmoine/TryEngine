a73e7b4c8ea4438ef7b52763571447018132ebc0
Tero <contact.tero@gmail.com>
2018-10-11 18:29:51 +0200

    Add World Update handling & fix problem when loading multiple level not erasing old data

--- src/TryEngine/CoreObject/TEWorld.cpp
+++ src/TryEngine/CoreObject/TEWorld.cpp
@@ -8,10 +8,15 @@
 TEWorld::TEWorld()
 {}
 
+std::weak_ptr<TEEntities> TEWorld::Entities() noexcept
+{
+    return m_entities;
+}
+
 void TEWorld::Load(TEEntities&& entities, Json::Value&& systems)
 {
     //entities
-    m_entities = entities;
+    m_entities = std::make_shared<TEEntities>(std::move(entities));
     {
         auto init = [](auto& c){ c.second.lock()->Initialize(); };
         for(auto& entity: entities.m_entities)
@@ -20,14 +25,14 @@ void TEWorld::Load(TEEntities&& entities, Json::Value&& systems)
         }
     }
     //systems
+    m_systems.clear();
     if(systems.size() > 0)
     {
         TESystemType systemType;
         TESystemID id;
         Json::Value system;
-        std::vector<std::weak_ptr<TESystem>> loadedSystems;
         const size_t typeCount = systems.size();
-        loadedSystems.reserve(typeCount);
+        m_systems.reserve(typeCount);
         for ( Json::Value::ArrayIndex index = 0; index < typeCount; ++index )
         {
             systemType = systems[index]["TypeId"].asString();
@@ -37,22 +42,36 @@ void TEWorld::Load(TEEntities&& entities, Json::Value&& systems)
             {
                 system = systems[index]["systems"][i];
                 id = system["system-id"].asLargestUInt();
-                loadedSystems.push_back(it->second(id));
-                std::prev(loadedSystems.end())->lock()->Load(std::move(system["data"]));
+                m_systems.push_back(it->second(id));
+                std::prev(m_systems.end())->lock()->Load(std::move(system["data"]));
             }
         }
         auto init = [](auto& s){ s.lock()->Initialize(); };
-        std::for_each(loadedSystems.begin(), loadedSystems.end(), init);
+        std::for_each(m_systems.begin(), m_systems.end(), init);
     }
+}
+
+void TEWorld::Update()
+{
+    for(auto& w_sys: m_systems)
+    {
+        if(const auto& sys = w_sys.lock())
+        {
+            sys->Update();
+        }
+    }
+    auto element = std :: remove_if (m_systems.begin(), m_systems.end() , [](const std::weak_ptr<TESystem>& w_sys){ return w_sys.expired(); });
+    m_systems.erase(element, m_systems.end());
 }
 
 Json::Value TEWorld::Serialize() const
 {
     Json::Value world;
     {
+        std::cout << m_entities->m_entities.size() << " entities" << std::endl;
         Json::Value entities;
         Json::ArrayIndex entCounter = 0;
-        for(const auto& entity : m_entities.m_entities)
+        for(const auto& entity : m_entities->m_entities)
         {
             entities[entCounter++] = entity->Serialize();
         }
@@ -68,7 +87,6 @@ Json::Value TEWorld::Serialize() const
 TEWorldStream::TEWorldStream(std::string&& file): m_file(file), m_world(std::make_shared<TEWorld>())
 {
     Load();
-    Save("Resources/worlds/default1.world.json");
 }
 
 void TEWorldStream::Load(const std::string& file)
